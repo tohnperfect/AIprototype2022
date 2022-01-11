c3b1fcad36dd21e3e5e4508ad4559cfc3c84e84d
tohnperfect <thanapong.intharah@gmail.com>
2022-01-11 09:52:24 +0700

    basic argparse

--- python_script_101.py
+++ python_script_101.py
@@ -1,5 +1,33 @@
+import argparse #สำหรับ รับ input จากภายนอก
+import subprocess #สำหรับ รัน terminal command
+
+#import flask #สำหรับ ทำ web app และ web service api
+
+
 def print_other():
     print('something else')
 
 if __name__ == "__main__":
-    print('test main function')
+    parser = argparse.ArgumentParser(description='test program to learn about argparse and subprocess')
+    parser.add_argument(
+        'm', 
+        type=int,
+        help='value of M positional argument')
+
+    parser.add_argument(
+        '--x', 
+        type=int,
+        help='value of x')
+
+    parser.add_argument(
+        '--yval',
+        type=int,
+        default=3,
+        help='value of y')
+        
+    args = parser.parse_args()
+    
+    x = args.x
+    y = args.yval
+    print(f'M = {args.m}')
+    print(f'calculate {x} x {y} = {x*y}')
