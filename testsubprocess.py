cb9860ef680ba50753c5297328e6049b4e2cccb6
tohnperfect <thanapong.intharah@gmail.com>
2022-01-18 09:55:27 +0700

    hw answer

--- testsubprocess.py
+++ testsubprocess.py
@@ -10,9 +10,13 @@ if __name__ == "__main__":
     ## use output of subprocess
     pro = subprocess.Popen(["ls", "-l"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = pro.communicate()
-    print(out)
+    print(out.decode('utf-8'))
 
 
     ##HW ให้ print เฉพาะ ตัวเลขผลลัพธ์การคูณ
+    print('start HW')
     for i in [2,5,6,8]:
-        subprocess.run(["python", "python_script_101.py", "9", "--x", f"{i}", "--yval", "2"])
+        process = subprocess.Popen(["python", "python_script_101.py", "9", "--x", f"{i}", "--yval", "2"],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = process.communicate()
+        print(out.split()[-1].decode('utf-8'))
